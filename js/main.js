9f659da49d9defd2708a085191cab8608c2acb30
Argyrios Tsagkalidis <arg.tsag@gmail.com>
2025-03-14 13:59:29 +0200

    Update: finished all sections except from projects

--- js/main.js
+++ js/main.js
@@ -77,16 +77,16 @@
     }, {offset: '80%'});
 
 
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
+    // Projects isotope and filter
+    var projectsIsotope = $('.projects-container').isotope({
+        itemSelector: '.projects-item',
         layoutMode: 'fitRows'
     });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
+    $('#projects-flters li').on('click', function () {
+        $("#projects-flters li").removeClass('active');
         $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        projectsIsotope.isotope({filter: $(this).data('filter')});
     });
     
     
