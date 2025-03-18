1ea35f2fe9309ba0e86279ff4b970ce8344a7b64
Argyrios Tsagkalidis <arg.tsag@gmail.com>
2025-03-18 22:11:02 +0200

    BugFix: primary mail server didn't work

--- mail/contact.js
+++ mail/contact.js
@@ -19,61 +19,71 @@ $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
 		message: message
 	});
 
-	// First check if primary server is responsive
 	var primaryServer = "https://smtp.myremoteserver.onedice.org/send_email";
-	var backupServer = "https://smtp.myhomeserver.onedice.org:2112/send_email";
+	var reduntancyServer = "https://smtp.myhomeserver.onedice.org:2112/send_email";
 	
-	// Set a timeout for the primary server test
-	var serverCheckTimeout = 2000; // 2 seconds timeout
-	
-	// Create a test request to the primary server
-	var serverTest = $.ajax({
+	// Send to primary server with a short timeout
+	$.ajax({
 		url: primaryServer,
-		type: "HEAD",
-		timeout: serverCheckTimeout
-	});
-	
-	// Handle the result of the server test
-	serverTest.done(function() {
-		// Primary server is up, send the actual request
-		sendEmailRequest(primaryServer);
-	}).fail(function() {
-		// Primary server check failed, use backup server
-		console.log("Primary server not responding, using backup server");
-		sendEmailRequest(backupServer);
-	});
-	
-	// Function to send the actual email request
-	function sendEmailRequest(serverUrl) {
-		$.ajax({
-		url: serverUrl,
 		type: "POST",
 		data: requestData,
 		contentType: "application/json",
+		timeout: 3000, // 3 second timeout for primary server
 		success: function () {
-			$('#success').html("<div class='alert alert-success'>");
-			$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-			.append("</button>");
-			$('#success > .alert-success')
-			.append("<strong>Message sent successfully!</strong>");
-			$('#success > .alert-success')
-			.append('</div>');
-			$('#contactForm').trigger("reset");
+		showSuccess();
 		},
-		error: function () {
-			$('#success').html("<div class='alert alert-danger'>");
-			$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-			.append("</button>");
-			$('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-			$('#success > .alert-danger').append('</div>');
-			$('#contactForm').trigger("reset");
+		error: function (jqXHR, textStatus, errorThrown) {
+		console.log("Primary server failed: " + textStatus + ". Trying reduntancy server...");
+		
+		// If primary server fails, try reduntancy server
+		$.ajax({
+			url: reduntancyServer,
+			type: "POST",
+			data: requestData,
+			contentType: "application/json",
+			success: function () {
+			showSuccess();
+			},
+			error: function () {
+			showError(name);
+			},
+			complete: function () {
+			enableButton();
+			}
+		});
 		},
 		complete: function () {
-			setTimeout(function () {
-			$this.prop("disabled", false);
-			}, 1000);
+		// Only enable the button if we're not going to the reduntancy server
+		if (this.success) {
+			enableButton();
 		}
-		});
+		}
+	});
+	
+	function showSuccess() {
+		$('#success').html("<div class='alert alert-success'>");
+		$('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+		.append("</button>");
+		$('#success > .alert-success')
+		.append("<strong>Message sent successfully!</strong>");
+		$('#success > .alert-success')
+		.append('</div>');
+		$('#contactForm').trigger("reset");
+	}
+	
+	function showError(name) {
+		$('#success').html("<div class='alert alert-danger'>");
+		$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+		.append("</button>");
+		$('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+		$('#success > .alert-danger').append('</div>');
+		$('#contactForm').trigger("reset");
+	}
+	
+	function enableButton() {
+		setTimeout(function () {
+		$this.prop("disabled", false);
+		}, 1000);
 	}
 	},
 	filter: function () {
